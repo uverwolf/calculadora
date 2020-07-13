30ac03f26b6a159afc8d1845da1a60412c17f140
Leoh <leonel.yam.meza@gmail.com>
2020-07-13 15:49:00 -0400

    Prueba calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,65 @@
+
+@SuppressWarnings("serial")
+public class Calculadora implements java.io.Serializable {
+	protected double operadorUno,operadorDos,resultado;
+	private String operador;
+	
+	public Calculadora() {
+		
+	}
+	public Calculadora(double n1,double n2,String operadorI) {
+		operadorUno =n1;
+		operadorDos=n2;
+		operador=operadorI;
+		if(operador=="+") {
+			this.setResultado(getOperadorUno()+getOperadorDos());
+			System.out.println(resultado);
+			}
+	}
+	public double getOperadorUno() {
+		return operadorUno;
+	}
+	public void setOperadorUno(double operadorUno) {
+		this.operadorUno = operadorUno;
+	}
+	public double getOperadorDos() {
+		return operadorDos;
+	}
+	public void setOperadorDos(double operadorDos) {
+		this.operadorDos = operadorDos;
+	}
+	public double getResultado() {
+		return resultado;
+	}
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+	public String getOperador() {
+		return operador;
+	}
+	public void setOperador(String operador) {
+		this.operador = operador;
+	}
+	
+	public void realizarCalculo(double operador1,double operador2, String Operador) {
+		switch (Operador) {
+		case "+":
+			setResultado(operador1+operador2);
+			System.out.println("La suma es : "+getResultado());
+			break;
+		case "-":
+			setResultado(operador1-operador2);
+			System.out.println("La resta es : "+getResultado());
+			break;
+		case "*":
+			setResultado(operador1*operador2);
+			System.out.println("La multiplicacion es : "+getResultado());
+			break;
+		case "/":
+			setResultado(operador1/operador2);
+			System.out.println("La division es : "+getResultado());
+		default:
+			break;
+		}
+	}
+}
---
+++ src/TestCalculadora.java
@@ -0,0 +1,13 @@
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculadora calculadora = new Calculadora(10.5,5.2,"+");
+		calculadora.realizarCalculo(5,2,"*");
+		calculadora.realizarCalculo(1,100,"+");
+		calculadora.realizarCalculo(9,3,"-");
+		calculadora.realizarCalculo(10,2,"/");
+	}
+
+}
